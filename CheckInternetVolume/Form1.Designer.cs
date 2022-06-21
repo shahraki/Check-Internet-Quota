3dddde394929dd2cb700d6a7bdd14fbcbcbca2c1
shahraki <shahram.shahraki@gmail.com>
2022-06-21 10:59:48 +0430

    first commit

---
+++ CheckInternetVolume/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace CheckInternetVolume
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unregisterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.screensToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quotaLabel = new System.Windows.Forms.Label();
+            this.label_username = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Internet Quata";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.registerToolStripMenuItem,
+            this.unregisterToolStripMenuItem,
+            this.usersToolStripMenuItem,
+            this.screensToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(129, 136);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.ShowToolStripMenuItem_Click);
+            // 
+            // registerToolStripMenuItem
+            // 
+            this.registerToolStripMenuItem.Name = "registerToolStripMenuItem";
+            this.registerToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.registerToolStripMenuItem.Text = "Register";
+            this.registerToolStripMenuItem.Click += new System.EventHandler(this.RegisterToolStripMenuItem_Click);
+            // 
+            // unregisterToolStripMenuItem
+            // 
+            this.unregisterToolStripMenuItem.Name = "unregisterToolStripMenuItem";
+            this.unregisterToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.unregisterToolStripMenuItem.Text = "Unregister";
+            this.unregisterToolStripMenuItem.Click += new System.EventHandler(this.UnregisterToolStripMenuItem_Click);
+            // 
+            // usersToolStripMenuItem
+            // 
+            this.usersToolStripMenuItem.Name = "usersToolStripMenuItem";
+            this.usersToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.usersToolStripMenuItem.Text = "Users";
+            // 
+            // screensToolStripMenuItem
+            // 
+            this.screensToolStripMenuItem.Name = "screensToolStripMenuItem";
+            this.screensToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.screensToolStripMenuItem.Text = "Screens";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // quotaLabel
+            // 
+            this.quotaLabel.AutoSize = true;
+            this.quotaLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.quotaLabel.Font = new System.Drawing.Font("Calibri", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.quotaLabel.ForeColor = System.Drawing.Color.White;
+            this.quotaLabel.Location = new System.Drawing.Point(0, 0);
+            this.quotaLabel.Name = "quotaLabel";
+            this.quotaLabel.Size = new System.Drawing.Size(222, 82);
+            this.quotaLabel.TabIndex = 1;
+            this.quotaLabel.Text = "000.00";
+            this.quotaLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.quotaLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label_username
+            // 
+            this.label_username.AutoSize = true;
+            this.label_username.Font = new System.Drawing.Font("CalliGravity", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_username.ForeColor = System.Drawing.Color.White;
+            this.label_username.Location = new System.Drawing.Point(228, 45);
+            this.label_username.Name = "label_username";
+            this.label_username.Size = new System.Drawing.Size(0, 31);
+            this.label_username.TabIndex = 2;
+            this.label_username.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(517, 80);
+            this.Controls.Add(this.label_username);
+            this.Controls.Add(this.quotaLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Check Internet Volume";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unregisterToolStripMenuItem;
+        private System.Windows.Forms.Label quotaLabel;
+        private System.Windows.Forms.ToolStripMenuItem screensToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usersToolStripMenuItem;
+        private System.Windows.Forms.Label label_username;
+    }
+}
+
---
+++ CheckInternetVolume/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net;
+using System.IO;
+using System.Reflection;
+
+namespace CheckInternetVolume
+{
+    public partial class Form1 : Form
+    {
+        /* private static HttpClientHandler handler = new HttpClientHandler();
+         private static readonly HttpClient client = new HttpClient(handler);*/
+        private string ScreenName = "";
+        private static readonly int LimitValume = 40;
+        private static readonly int TimePeriod = 5; //in mituntes
+        private static readonly string[] quotaEqualZeroMessages = new string[2] { "Something Went Wrong or Reached Quota Limit", "The return value is " };
+        private static readonly string[] quotaOutOfLimitMessages = new string[2] { "Internet is Out of Limit", "The Quota is less than " };
+        private static readonly string[] quotaInformationMessages = new string[2] { "Internet Quota Information", "The remaining Quota is " };
+        private static readonly string[] InternetRegistrationSuccessMessages = new string[2] { "Internet Registration Information", "You have been registerd successfully" };
+        private static readonly string[] InternetRegistrationUnsuccessMessages = new string[2] { "Internet Registration Information", "You have not been registerd successfully or already registered" };
+        private static readonly string[] InternetUnRegistrationSuccessMessages = new string[2] { "Internet Unregistration Information", "You have been Unregisterd successfully" };
+        private static readonly string[] InternetUnegistrationUnsuccessMessages = new string[2] { "Internet Unregistration Information", "You have not been Unregisterd successfully" };
+        //private static string[] Users = { "noBody", "name=00a3a6f5d8a842114809baffbeff80ed", "word=003a224148da6adb551201e7cf71031e 00a667d0545699cb0ca7f5db1e601d9e" };
+        private static string[] Users = { };
+        private static string UsersName = "";
+        //private static string ResigtrationURL = "https://192.168.26.1:442/tr_auth_cnf.html?command=Register&name=00a3a6f5d8a842114809baffbeff80ed&word=003a224148da6adb551201e7cf71031e 00a667d0545699cb0ca7f5db1e601d9e";
+        //private static string ResigtrationURL = "https://192.168.26.1:442/kta.cgi?command=Register&name=00a3a6f5d8a842114809baffbeff80ed&word=003a224148da6adb551201e7cf71031e%2000a667d0545699cb0ca7f5db1e601d9e";
+        private static string ResigtrationURL = "https://192.168.26.1:442/api/v3/auth/command?command=Register&name=shahraki&word=shahraki=401984";
+        //private static string InformationURL = "https://192.168.26.1:442/tr_auth_cnf.html?command=View Account&name=00a3a6f5d8a842114809baffbeff80ed&word=003a224148da6adb551201e7cf71031e 00a667d0545699cb0ca7f5db1e601d9e";
+        //private static string InformationURL = "https://192.168.26.1:442/kta.cgi?command=View%20Account&name=00a3a6f5d8a842114809baffbeff80ed&word=003a224148da6adb551201e7cf71031e%2000a667d0545699cb0ca7f5db1e601d9e";
+        private static string InformationURL = "https://192.168.26.1:442/kta.cgi?command=View%20Account&name=shahraki&word=shahraki=401984";
+        //private static string UnresigtrationURL = "https://192.168.26.1:442/tr_auth_cnf.html?command=Unregister&name=00a3a6f5d8a842114809baffbeff80ed&word=003a224148da6adb551201e7cf71031e 00a667d0545699cb0ca7f5db1e601d9e";
+        //private static string UnresigtrationURL = "https://192.168.26.1:442/kta.cgi?command=Unregister&name=00a3a6f5d8a842114809baffbeff80ed&word=003a224148da6adb551201e7cf71031e%2000a667d0545699cb0ca7f5db1e601d9e";
+        private static string UnresigtrationURL = "https://192.168.26.1:442/api/v3/auth/command?command=Unregister&name=shahraki&word=shahraki=401984";
+
+        //public static string[] QuotaInformationMessages => quotaInformationMessages;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoWorkAsyncInfiniteLoop();
+        }
+
+        private async Task<double> ReceiveInternetQuota()
+        {
+            var handler = new HttpClientHandler();
+            handler.UseProxy = false;
+            HttpClient client = new HttpClient(handler);
+            //HttpClient client = new HttpClient();
+            ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(delegate { return true; });
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            try
+            {
+                string findQuotaString = "var TrafficQuota = new Array([\"Days\",\"Total\",\"1\",\"1024 mbytes\",\"";
+                int lengthOfFindQuotaString = findQuotaString.Length;
+                char[] quotac = new char[6];
+                var response = await client.GetStringAsync(InformationURL);
+                //int Tindex = response.IndexOf("var TrafficQuota = new Array([\"Days\",\"Total\",\"1\",\"300 mbytes\",\"");
+                //int Tindex = response.IndexOf("var TrafficQuota = new Array([\"Days");
+                int Tindex = response.IndexOf(findQuotaString) + lengthOfFindQuotaString;
+                response.CopyTo(Tindex , quotac, 0, 6);
+                string quotas = new string(quotac);
+                double quota = new double();
+                //quota = Convert.ToDouble(quotas);
+                quota = Convert.ToDouble(System.Text.RegularExpressions.Regex.Match(quotas, @"[-+]?([0-9]*\.[0-9]+|[0-9]+)").Value);
+                quotaLabel.Text = quota.ToString(); 
+                label_username.Text = " of " + UsersName;
+                return quota;
+                /*if (quota < 500)
+                {
+                    notifyIcon1.ShowBalloonTip(2000, "Out of Quota", "The Quota is less than " + quotas, ToolTipIcon.Info);
+                }*/
+            }
+            catch (Exception ee)
+            {
+                //MessageBox.Show(ee.ToString());
+                return 0;
+            }
+            /*finally
+            {
+                await client.DeleteAsync("https://192.168.26.1:442/tr_auth_cnf.html?command=View Account&name=00a3a6f5d8a842114809baffbeff80ed&word=003a224148da6adb551201e7cf71031e 00a667d0545699cb0ca7f5db1e601d9e");
+                handler.UseProxy = true;
+            }*/
+
+        }
+
+        static async Task<string> RegisterInternet()
+        {
+            var handler = new HttpClientHandler();
+            handler.UseProxy = false;
+            HttpClient client = new HttpClient(handler);
+
+            ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(delegate { return true; });
+            //HttpClient client = new HttpClient();
+            try
+            {
+                //handler.UseProxy = false;
+                var response = await client.GetStringAsync(ResigtrationURL);
+                if (response.Contains("Register/Unregister was accomplished successfully"))
+                    return "yes";
+                else
+                    return "no";
+            }
+            catch (Exception)
+            {
+                //MessageBox.Show(ee.ToString());
+                return "no";
+            }
+
+        }
+
+        static async Task<string> UnRegisterInternet()
+        {
+            var handler = new HttpClientHandler();
+            handler.UseProxy = false;
+            HttpClient client = new HttpClient(handler);
+            ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(delegate { return true; });
+            //HttpClient client = new HttpClient();
+            try
+            {
+                //handler.UseProxy = false;
+                var response = await client.GetStringAsync(UnresigtrationURL);
+                if (response.Contains("Register/Unregister was accomplished successfully"))
+                    return "yes";
+                else
+                    return "no";
+            }
+            catch (Exception)
+            {
+                //MessageBox.Show(ee.ToString());
+                return "no";
+            }
+
+        }
+
+        private async Task DoWorkAsyncInfiniteLoop()
+        {
+            while (true)
+            {
+                // do the work in the loop
+                double quota = await ReceiveInternetQuota();
+                //quotaLabel.Text = quota.ToString();
+                // update the UI
+                if (quota == 0)
+                    notifyIcon1.ShowBalloonTip(2000, quotaEqualZeroMessages[0], quotaEqualZeroMessages[1] + quota.ToString(), ToolTipIcon.Error);
+                else if (quota < LimitValume)
+                    notifyIcon1.ShowBalloonTip(2000, quotaInformationMessages[0], quotaInformationMessages[1] + quota.ToString(), ToolTipIcon.Warning);
+                // don't run again for at least TimePeriod * 60 * 1000 milliseconds
+                await Task.Delay(TimePeriod * 60 * 1000);
+            }
+        }
+
+        private async void ShowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            double quota = await ReceiveInternetQuota();
+            if (quota == 0)
+                notifyIcon1.ShowBalloonTip(2000, quotaEqualZeroMessages[0], quotaEqualZeroMessages[1] + quota.ToString(), ToolTipIcon.Error);
+            else if (quota < LimitValume)
+                notifyIcon1.ShowBalloonTip(2000, quotaOutOfLimitMessages[0], quotaOutOfLimitMessages[1] + quota.ToString(), ToolTipIcon.Warning);
+            else
+                notifyIcon1.ShowBalloonTip(2000, quotaInformationMessages[0], quotaInformationMessages[1] + quota.ToString(), ToolTipIcon.Info);
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private async void RegisterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //double quota = await registerInternet();
+            //string isRegisterd = await registerInternet();
+            await ReceiveInternetQuota();
+            if (await RegisterInternet() == "yes")
+                notifyIcon1.ShowBalloonTip(2000, InternetRegistrationSuccessMessages[0], InternetRegistrationSuccessMessages[1], ToolTipIcon.Info);
+            else
+                notifyIcon1.ShowBalloonTip(2000, InternetRegistrationUnsuccessMessages[0], InternetRegistrationUnsuccessMessages[1], ToolTipIcon.Error);
+        }
+
+        private async void UnregisterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            await ReceiveInternetQuota();
+            if (await UnRegisterInternet() == "yes")
+                notifyIcon1.ShowBalloonTip(2000, InternetUnRegistrationSuccessMessages[0], InternetUnRegistrationSuccessMessages[1], ToolTipIcon.Info);
+            else
+                notifyIcon1.ShowBalloonTip(2000, InternetUnegistrationUnsuccessMessages[0], InternetUnegistrationUnsuccessMessages[1], ToolTipIcon.Error);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ///locating right screen to show
+            int x = Screen.PrimaryScreen.Bounds.Right;
+            //var screen = Screen.AllScreens;
+            foreach (var screen in Screen.AllScreens)
+            {
+                (contextMenuStrip1.Items[4] as ToolStripMenuItem).DropDownItems.Add(screen.DeviceName);
+            }
+            (contextMenuStrip1.Items[4] as ToolStripMenuItem).DropDownItemClicked += Screen_DropDownItemClicked;
+            //x = screen[1].Bounds.Left;
+            this.Location = new Point(x, 0);
+
+            ///locating right user to use to
+            //string Dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase);
+            string filenamelocation = Environment.CurrentDirectory + @"\credentials.txt";
+            //string filenamelocation = Path.Combine(Dir, "commitMessageTemplate.txt");
+            if (File.Exists(filenamelocation))
+            {
+                Users = File.ReadAllLines(filenamelocation, Encoding.UTF8);
+                UsersName = Users[0];
+                for (int i = 0; i < Users.Length; i = i + 3)
+                {
+                    string User = Users[i];
+                    (contextMenuStrip1.Items[3] as ToolStripMenuItem).DropDownItems.Add(User);
+                }
+                (contextMenuStrip1.Items[3] as ToolStripMenuItem).DropDownItemClicked += Users_DropDownItemClicked;
+            }
+            else
+            {
+                (contextMenuStrip1.Items[3] as ToolStripMenuItem).DropDownItems.Add("Default User");
+            }
+             
+        }
+
+        private void Users_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            string User = e.ClickedItem.ToString();
+            UsersName = User;
+            //ResigtrationURL = "https://192.168.26.1:442/tr_auth_cnf.html?command=Register&name=00a3a6f5d8a842114809baffbeff80ed&word=003a224148da6adb551201e7cf71031e 00a667d0545699cb0ca7f5db1e601d9e";
+            ResigtrationURL = string.Concat("https://192.168.26.1:442/api/v3/auth/command?command=Register&", Users[Array.IndexOf(Users, User) + 1], "&", Users[Array.IndexOf(Users, User) + 2]);
+            UnresigtrationURL = string.Concat("https://192.168.26.1:442/api/v3/auth/command?command=Unregister&", Users[Array.IndexOf(Users, User) + 1], "&", Users[Array.IndexOf(Users, User) + 2]);
+            InformationURL = string.Concat("https://192.168.26.1:442/kta.cgi?command=View%20Account&", Users[Array.IndexOf(Users, User) + 1], "&", Users[Array.IndexOf(Users, User) + 2]);
+        }
+
+        private void Screen_DropDownItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            ScreenName = e.ClickedItem.ToString();
+            //var screen = Screen.AllScreens;
+            int x = new int();
+            foreach (var screen in Screen.AllScreens)
+            {
+                if (screen.DeviceName == e.ClickedItem.ToString())
+                {
+                    x = screen.Bounds.Left;
+                    this.Location = new Point(x, 0);
+                    this.Focus();
+                    break;
+                }
+            }
+
+        }
+
+    }
+}
